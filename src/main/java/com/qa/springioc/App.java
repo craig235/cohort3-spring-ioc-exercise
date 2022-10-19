7d0dddf04be88d9b6dbf2635b86cc501961f44d1
craig235 <crawells@gmail.com>
2022-10-19 14:29:31 +0100

    no comment

--- src/main/java/com/qa/springioc/App.java
+++ src/main/java/com/qa/springioc/App.java
@@ -12,15 +12,24 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        System.out.println("------");
+        System.out.println("--- 1 ---");
 
         Customer cus1 = context.getBean("cus1", Customer.class);
         System.out.println(cus1.toString());
 
+
+        System.out.println("--- 2 ---");
+
         Order order1 = context.getBean("order1", Order.class);
         System.out.println(order1.toString());
 
+        System.out.println("--- 3 ---");
+
         Order order2 = context.getBean("order2", Order.class);
         System.out.println(order2.toString());
+
+        System.out.println("--- 4 ---");
+        System.out.println(cus1.getOrdersList().get(0) == order1);
+        System.out.println(cus1.getOrdersList().get(1) == order1);
     }
 }
